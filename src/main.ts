9aecbd87245cbcb4843fba514a99ddf81dc06444
wuTree <664065745@qq.com>
2023-09-18 23:42:01 +0800

    feat:增加导航目录

--- src/main.ts
+++ src/main.ts
@@ -42,5 +42,33 @@ document.addEventListener("DOMContentLoaded", () => {
             return
         }
     })
+
 })
+
+let nav = document.getElementById("post-navigation-wrapper");
+if (nav) {
+    // 监听 post-page 的变化，动态调整 post-page 的位置
+    let content = document.getElementById("post-content");
+    if (content) {
+
+        let children = content.children;
+        // 获取所有tag为 h1-h6 的元素，并生成导航 添加到 #post-navigation
+        for (let i = 0; i < children.length; i++) {
+
+            let child = children[i];
+            let tagName = child.tagName;
+            if (tagName === "H1" || tagName === "H2" || tagName === "H3" || tagName === "H4" || tagName === "H5" || tagName === "H6") {
+                let id = child.id;
+
+                let li = document.createElement(tagName);
+                let a = document.createElement("a");
+                a.setAttribute("href", "#" + id);
+                a.innerText = child.innerHTML;
+                li.appendChild(a);
+                nav?.appendChild(li);
+            }
+        }
+    }
+}
+
 
